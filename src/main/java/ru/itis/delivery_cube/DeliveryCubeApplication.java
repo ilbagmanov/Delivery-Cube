2a9787a1919126758045bd2986a863e2d5b31a37
BagmanovID <i.bagmanov@id-mt.ru>
2022-02-17 10:26:28 +0300

    version: 0.1

---
+++ src/main/java/ru/itis/delivery_cube/DeliveryCubeApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.delivery_cube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryCubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryCubeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itis/delivery_cube/DeliveryCubeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.delivery_cube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryCubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
